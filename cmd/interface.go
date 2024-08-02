6c2461fd8449960e6316613b5300e62e023cc1d4
kelche <kelcheone@gmail.com>
2024-08-02 14:34:12 +0300

    feat: get the default interface of a given machine

---
+++ cmd/interface.go
@@ -0,0 +1,42 @@
+package sniffer
+
+import (
+	"fmt"
+	"net"
+)
+
+func getDefaultInterface() (string, error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+
+	for _, iface := range interfaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			if ip.To4() != nil {
+				return iface.Name, nil
+			}
+		}
+	}
+	return "", fmt.Errorf("no active non-loopback interface found")
+}
